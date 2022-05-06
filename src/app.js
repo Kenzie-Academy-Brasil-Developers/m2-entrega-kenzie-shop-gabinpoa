07900866c330654792a30d4cfc44d348fb80d1d0
gabinpoa <gabrielnpoa@gmail.com>
2022-05-06 16:20:48 -0300

    Editing styles and adding the reviews feature

--- src/app.js
+++ src/app.js
@@ -3,20 +3,27 @@ class App {
     let response = await fetch("https://m2-kenzie-shop.herokuapp.com/products")
       .then((res) => res.json())
       .then((data) => data.products);
-    response.forEach((product) => {
+    await response.forEach((product) => {
       this.createProductTemplate(product);
     });
   }
   static createProductTemplate(product) {
     let containerProduct = document.createElement("div");
     let img = document.createElement("img");
-    let description = document.createElement("p");
+    let reviews = document.createElement("ul");
+    let productName = document.createElement("p");
     let price = document.createElement("span");
+    let buttonBuy = document.createElement("button");
 
     containerProduct.id = product.id;
     img.src = `https://kenzie-academy-brasil.gitlab.io/fullstack/frontend/modulo2/sprint3/img/consumindo-api-produtos/${product.id}/Image.png`;
     img.alt = product.productName;
-    description.innerText = product.productName;
+    for (let i = 0; i < product.reviews; i++) {
+      reviews.append(document.createElement("li"));
+    }
+    let reducedName = product.productName.substring(0, 30);
+    productName.innerText = reducedName;
+    buttonBuy.innerText = "Comprar";
     if (!product.promotionStatus) {
       price.innerText = "R$" + product.price.productPrice;
       price.classList = "final-price";
@@ -32,7 +39,8 @@ class App {
 
       price.append(initialPrice, promotionPrice);
     }
-    containerProduct.append(img, description, price);
+
+    containerProduct.append(img, reviews, productName, price, buttonBuy);
     document.getElementById("products").append(containerProduct);
   }
 }
